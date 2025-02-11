8b833f9cf3e317ac460dc542f8917b8d0b414e04
killianlf <69599838+killianlf@users.noreply.github.com>
2025-02-11 11:26:11 +0100

    Add files via upload

---
+++ imds5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Set the metadata service URL
+metadata_url="http://169.254.169.254/latest/api/token"
+# Set the TTL (Time-To-Live) for the token in seconds (e.g., 21600 seconds = 6 hours)
+ttl_seconds=21600
+# Get the IMDSv2 token
+tok=$(curl -X PUT "$metadata_url" -H "X-aws-ec2-metadata-token-ttl-seconds: $ttl_seconds")
+metadata="http://169.254.169.254/latest/meta-data/iam/security-credentials/"
+ec2=$(curl  "$metadata" -H "X-aws-ec2-metadata-token: $tok")
+echo "$ec2"
+ 
+token=$(curl "http://169.254.169.254/latest/meta-data/iam/security-credentials/$ec2/" -H "X-aws-ec2-metadata-token: $tok")
+echo "$token" >> /tmp/token
+ 
+curl "http://169.254.169.254/latest/meta-data/iam/security-credentials/$ec2/" -H "X-aws-ec2-metadata-token: $tok"
